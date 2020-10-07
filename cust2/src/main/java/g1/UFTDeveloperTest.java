b9619081524154866952924cfd64b37e0000ccfe
demo <admin@default.com>
2020-10-07 12:45:25 -0500

    Initial Commit
    Right after recording, with no modifications

---
+++ cust2/src/main/java/g1/UFTDeveloperTest.java
@@ -0,0 +1,98 @@
+package g1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import com.hp.lft.sdk.*;
+import com.hp.lft.sdk.web.*;
+import com.hp.lft.verifications.*;
+
+import unittesting.*;
+
+public class UFTDeveloperTest extends UnitTestClassBase {
+
+    public UFTDeveloperTest() {
+        //Change this constructor to private if you supply your own public constructor
+    }
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        instance = new UFTDeveloperTest();
+        globalSetup(UFTDeveloperTest.class);
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        globalTearDown();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() throws GeneralLeanFtException {
+
+        Browser browser = BrowserFactory.launch(BrowserType.CHROME);
+        browser.navigate("http://advantageonlineshopping.com/");
+
+        Link tabletsCategoryTxtLink = browser.describe(Link.class, new LinkDescription.Builder()
+                .innerText("TABLETS")
+                .tagName("SPAN").build());
+        tabletsCategoryTxtLink.click();
+
+        WebElement hPEliteX21011G1TabletWebElement = browser.describe(WebElement.class, new WebElementDescription.Builder()
+                .innerText("HP Elite x2 1011 G1 Tablet")
+                .tagName("A").build());
+        hPEliteX21011G1TabletWebElement.click();
+
+        EditField quantityEditField = browser.describe(EditField.class, new EditFieldDescription.Builder()
+                .name("quantity")
+                .tagName("INPUT")
+                .type("text").build());
+
+        quantityEditField.setValue("2");
+
+        Button saveToCartButton = browser.describe(Button.class, new ButtonDescription.Builder()
+                .buttonType("submit")
+                .name("ADD TO CART")
+                .tagName("BUTTON").build());
+        saveToCartButton.click();
+
+        Button checkOutBtnButton = browser.describe(Button.class, new ButtonDescription.Builder()
+                .accessibilityName("")
+                .buttonType("submit")
+                .index(0)
+                .name(new RegExpProperty("CHECKOUT  \\(\\$\\d.*"))
+                .role("button")
+                .tagName("BUTTON").build());
+        Verify.areEqual("CHECKOUT ($2,558.00)", checkOutBtnButton.getInnerText(), "Verification", "Verify property: innerText");
+
+        Link shoppingCartLink = browser.describe(Link.class, new LinkDescription.Builder()
+                .accessibilityName("ShoppingCart")
+                .innerText(new RegExpProperty("\\d"))
+                .role("link")
+                .tagName("A").build());
+        shoppingCartLink.click();
+
+        WebElement webElement = browser.describe(WebElement.class, new WebElementDescription.Builder()
+                .accessibilityName("")
+                .className("removeProduct iconCss iconX")
+                .innerText("")
+                .tagName("DIV")
+                .index(0).build());
+        webElement.click();
+
+        Link hOMELink = browser.describe(Link.class, new LinkDescription.Builder()
+                .innerText("HOME")
+                .tagName("A").build());hOMELink.click();
+
+    }
+
+}
